0d9163c26146af7fc9ed2f57ac74ce6bae60b611
Moses Yoo <juyoung.yoo@supernal.aero>
2025-09-11 15:42:16 -0700

    Update folder name.

--- mini_grep/src/main.rs
+++ mini_grep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use mini_grep::Config;
 
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
@@ -9,9 +9,7 @@ fn main() {
         process::exit(1);
     });
 
-    // --snip--
-
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = mini_grep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
